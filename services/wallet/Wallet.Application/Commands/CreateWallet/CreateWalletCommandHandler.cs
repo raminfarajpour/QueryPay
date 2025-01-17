58bc6b98921411665b2f6777777cb3bdb0748bc3
Ramin Farajpour <R.Farajpour@Alibaba.ir>
2025-01-17 16:57:37 +0330

    fix create command handler

--- services/wallet/Wallet.Application/Commands/CreateWallet/CreateWalletCommandHandler.cs
+++ services/wallet/Wallet.Application/Commands/CreateWallet/CreateWalletCommandHandler.cs
@@ -22,20 +22,12 @@ public class CreateWalletCommandHandler(
 
     public async Task Handle(CreateWalletCommand command, CancellationToken cancellationToken)
     {
-        // var wallet =
-        //     await eventStore.RehydrateAsync<Domain.WalletAggregate.Wallet>(
-        //         Guid.Parse("bf015df2-af66-4a72-b15e-8cebcd9bac7e"), cancellationToken: cancellationToken);
-
         var wallet = new Domain.WalletAggregate.Wallet();
         wallet.Create(
             initialBalance: new Money(command.InitialBalance),
             overUsedThreshold: new Money(command.OverUsedThreshold),
             new Owner(command.OwnerUserId, command.OwnerMobile)
         );
-        for (var i = 0; i < 150; i++)
-        {
-            wallet?.Deposit(new Money(3000L), new TransactionInfo("asd-asdasd", "asdasda", "test description"));
-        }
 
         await eventStore.PersistAsync(wallet, cancellationToken);
 
