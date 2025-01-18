fc0c408580cd85c38e9aaaf7b8b44720ef9ff606
Ramin Farajpour <R.Farajpour@Alibaba.ir>
2025-01-18 16:35:21 +0330

    fix rabbitmq connection factory

--- services/billing/Billing.Infrastructure/SeedWorks/Integration/MessageBroker/IRabbitMqConnectionFactory.cs
+++ services/billing/Billing.Infrastructure/SeedWorks/Integration/MessageBroker/IRabbitMqConnectionFactory.cs
@@ -2,7 +2,8 @@
 
 namespace Billing.Infrastructure.SeedWorks.Integration.MessageBroker;
 
-public interface IRabbitMqConnectionFactory
+public interface IRabbitMqConnectionFactory<TSetting> where TSetting : RabbitMqSetting,new()
 {
+    TSetting Settings { get; }
     Task<(IConnection Connection,List<RabbitMqExchangeSetting> ExchangeSettings)> CreateConnectionAsync(CancellationToken cancellationToken);
 }
